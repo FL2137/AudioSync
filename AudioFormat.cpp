e416e9eb29e560b6d69f851b2ead0f6185c9d2d9
XD67 <franek1519@gmail.com>
2023-12-25 15:11:11 +0100

    Create AudioFormat class

---
+++ AudioFormat.cpp
@@ -0,0 +1,4 @@
+#include "AudioFormat.hpp"
+
+
+
---
+++ AudioFormat.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <qdebug.h>
+#include <qaudioformat.h>
+#include <qaudio.h>
+#include <qbytearray.h>
+
+class AudioFormat {
+	
+
+};
