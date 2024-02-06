d067134c83540907996241b5df6f0c141c6ffb1b
FL2137 <franek1519@gmail.com>
2024-02-06 23:52:31 +0100

    Fix

--- TcpServer.hpp
+++ TcpServer.hpp
@@ -8,23 +8,42 @@
 using nlohmann::json;
 using boost::asio::ip::tcp;
 
+const std::string SERVER_ADDRESS = "192.168.1.109";
+const int PORT = 3005;
+
 class TcpConnection {
 
+};
+
+class TcpRequest {
+public:
+	std::string type;
+	int uid;
+	json data;
+
+
+	std::string stringify() {
+		json json;
+		json["type"] = type;
+		json["uid"] = uid;
+		json["data"] = data.dump();
+		return json.dump();
+	}
 };
 
 class TcpClient {
 
 public:
 
-	static void send(std::string request, std::string &response) {
+	static void send(TcpRequest request, std::string &response) {
 		boost::asio::io_context ioc;
 		tcp::socket socket(ioc);
 
-		socket.connect(tcp::endpoint(boost::asio::ip::make_address_v4("192.168.1.109"), 3005));
+		socket.connect(tcp::endpoint(boost::asio::ip::make_address_v4(SERVER_ADDRESS), PORT));
 		
 		boost::system::error_code error;
 
-		socket.write_some(boost::asio::buffer(request), error);
+		socket.write_some(boost::asio::buffer(request.stringify()), error);
 
 		if (error) {
 			qWarning() << error.message();
@@ -32,16 +51,13 @@ public:
 		
 		char readBuffer[512];
 
-		size_t size = socket.read_some(boost::asio::buffer(readBuffer), error);
+		size_t size = socket.read_some(boost::asio::buffer(readBuffer, 512), error);
 		
+		response = std::string(readBuffer, 512);
+
 		if (error) {
 			qWarning() << error.message();
 		}
-
-		response = std::string(readBuffer);
-
-		response.erase(response.end()-1); //for some reason one too much char is being sent
-
 		qDebug() << response;
 
 		socket.close();
