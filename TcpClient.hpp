56cade7cdb93695e4ec553c62b52786404bf4def
FL2137 <franek1519@gmail.com>
2024-02-10 14:42:25 +0100

    Moved TcpClient class to its own header

---
+++ TcpClient.hpp
@@ -0,0 +1,56 @@
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class TcpClient {
+
+public:
+
+	static void send(TcpRequest request, std::string& response) {
+		boost::asio::io_context ioc;
+		tcp::socket socket(ioc);
+
+		socket.connect(tcp::endpoint(boost::asio::ip::make_address_v4(SERVER_ADDRESS), PORT));
+
+		boost::system::error_code error;
+
+		socket.write_some(boost::asio::buffer(request.stringify()), error);
+
+		if (error) {
+			qWarning() << error.message();
+		}
+
+		char readBuffer[512];
+
+		size_t size = socket.read_some(boost::asio::buffer(readBuffer, 512), error);
+
+		response = std::string(readBuffer, 512);
+
+		if (error) {
+			qWarning() << error.message();
+		}
+		qDebug() << response;
+
+		socket.close();
+	}
+
+private:
+
+};
+
+
+class TcpRequest {
+public:
+	std::string type;
+	int uid;
+	json data;
+
+
+	std::string stringify() {
+		json json;
+		json["type"] = type;
+		json["uid"] = uid;
+		json["data"] = data.dump();
+		return json.dump();
+	}
+};
--- TcpServer.hpp
+++ TcpServer.hpp
@@ -82,8 +82,9 @@ private:
     tcp::socket _socket;
 };
 
+class TcpServer {
 
-
+};
 
 class TcpRequest {
 public:
@@ -101,38 +102,3 @@ public:
 	}
 };
 
-class TcpClient {
-
-public:
-
-	static void send(TcpRequest request, std::string &response) {
-		boost::asio::io_context ioc;
-		tcp::socket socket(ioc);
-
-		socket.connect(tcp::endpoint(boost::asio::ip::make_address_v4(SERVER_ADDRESS), PORT));
-		
-		boost::system::error_code error;
-
-		socket.write_some(boost::asio::buffer(request.stringify()), error);
-
-		if (error) {
-			qWarning() << error.message();
-		}
-		
-		char readBuffer[512];
-
-		size_t size = socket.read_some(boost::asio::buffer(readBuffer, 512), error);
-		
-		response = std::string(readBuffer, 512);
-
-		if (error) {
-			qWarning() << error.message();
-		}
-		qDebug() << response;
-
-		socket.close();
-	}
-
-private:
-
-};
