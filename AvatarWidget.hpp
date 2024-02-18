403de305be1ca25f142bbc9991a1c8f937b45b0a
FL2137 <franek1519@gmail.com>
2024-02-18 23:41:48 +0100

    Version for avatar of the local user

--- AvatarWidget.hpp
+++ AvatarWidget.hpp
@@ -1,11 +1,41 @@
 #pragma once
 #include <qwidget.h>
 #include <qstring.h>
+#include <string>
 #include <qdir.h>
+#include <qfile.h>
+
+#include "nlohmann/json.hpp"
+
+using nlohmann::json;
 
 class AvatarWidget : QWidget {
 
 public:
+
+	AvatarWidget(QWidget* parent = nullptr) : QWidget(parent) {
+		this->setObjectName("UserAvatar");
+
+		QFile settings(QDir::currentPath() + "/userSettings.json");
+
+		if (!settings.open(QIODevice::ReadOnly | QIODevice::Text)) {
+			QString path = QDir::currentPath() + "/x64/Debug/Avatars/userAvatar.png";
+			this->setStyleSheet("background-image: url(" + path + ")");
+		}
+		else {
+			std::string settingsData = settings.readAll().toStdString();
+			json setts;
+
+			if (json::accept(settingsData))
+				setts = json::parse(settingsData);
+			
+			QString path = QDir::currentPath() + "/x64/Debug/Avatars/" + QString::fromStdString(setts["avatar"].get<std::string>());
+			this->setStyleSheet("background-image: url(" + path + ")");
+		}
+
+
+		this->setGeometry(QRect(parent->geometry().width() - 210, 30, 100, 100));
+	}
 
 	AvatarWidget(QString file, QWidget* parent = nullptr) : QWidget(parent) {
 		this->setObjectName("avatar" + file);
