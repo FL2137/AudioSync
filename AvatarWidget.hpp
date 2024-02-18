c7695abfe457af9d2dbe64a2f319412996745736
FL2137 <franek1519@gmail.com>
2024-02-18 16:53:24 +0100

    AvatarWidget uses image passed to it

--- AvatarWidget.hpp
+++ AvatarWidget.hpp
@@ -1,14 +1,32 @@
 #pragma once
 #include <qwidget.h>
 #include <qstring.h>
-
+#include <qdir.h>
 
 class AvatarWidget : QWidget {
 
 public:
 
 	AvatarWidget(QString file, QWidget *parent = nullptr) : QWidget(parent) {
+		this->setObjectName("avatar"+file);
+		int n = 1;
 
+		QRect parentGeo = parent->geometry();
+
+		int pY = parentGeo.y();
+		int pX = parentGeo.x();
+
+		this->setGeometry(QRect(10, 10, 100, 100));
+
+
+		QString avatarPath = QDir::currentPath() + "/x64/Debug/Avatars/" + file + ".png";
+
+		this->setStyleSheet("background-image: url(" + avatarPath + ")");
+		this->show();//needed?
+	}
+
+	~AvatarWidget() {
+		this->destroy(true, true);
 	}
 
 };
