9ca7d476777f68898cccc4aaa5cfcc178c9ec25e
FL2137 <franek1519@gmail.com>
2024-02-18 16:05:06 +0100

    Custom AvatarWidget class

---
+++ AvatarWidget.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <qwidget.h>
+#include <qstring.h>
+
+
+class AvatarWidget : QWidget {
+
+public:
+
+	AvatarWidget(QString file, QWidget *parent = nullptr) : QWidget(parent) {
+
+	}
+
+};
