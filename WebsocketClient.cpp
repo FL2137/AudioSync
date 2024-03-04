3c5a6dba7658fb3acc49d241fbbac87bef52a18c
FL2137 <franek1519@gmail.com>
2024-03-04 23:40:57 +0100

    slots implementations

--- WebsocketClient.cpp
+++ WebsocketClient.cpp
@@ -7,4 +7,17 @@ WebsocketClient::WebsocketClient(const QUrl& url, QObject* parent) : QObject(par
 	socket.open(url);
 }
 
+void WebsocketClient::onConnected() {
+	qDebug() << "Websocket connected";
+	connect(&socket, &QWebSocket::textMessageReceived, this, &WebsocketClient::onTextMessageReceived);
 
+	//optional send
+	//socket.sendTextMessage("lalalalalalal");
+}
+
+void WebsocketClient::onTextMessageReceived(QString message) {
+	qDebug() << "WebsocketClient received: " << message;
+
+	//optional close
+	socket.close();
+}
