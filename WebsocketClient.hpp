bc1e1ad0847776dcfe3387df88c1395d40b717fa
FL2137 <franek1519@gmail.com>
2024-03-04 23:32:41 +0100

    Supposedly this provides 2 way stable communication

---
+++ WebsocketClient.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <qwebsocket.h>
+#include <qdebug.h>
+#include <qobject.h>
+#include <qstring.h>
+
+
+class WebsocketClient : public QObject {
+	Q_OBJECT
+
+public:
+	WebsocketClient(const QUrl& url, bool debug = false, QObject* parent = nullptr);
+			
+
+signals:
+	void closed();
+
+private slots:
+	void onConnected();
+	void onTextMessageReceived(QString message);
+
+private:
+	QWebSocket socket;
+	bool debug;
+};
