ef268035e6bb32fe80aba2406a2f486b8eecd9d5
FL2137 <franek1519@gmail.com>
2024-03-07 22:58:09 +0100

    works!

--- WebsocketClient.cpp
+++ WebsocketClient.cpp
@@ -1,23 +1,27 @@
 #include "WebsocketClient.hpp"
 
+#include <qnetworkrequest.h>
+
 WebsocketClient::WebsocketClient(const QUrl& url, QObject* parent) : QObject(parent) {
-	qDebug() << "Websocket server: " << url;
+	QUrl u("ws://192.168.1.109:3005");
+	qDebug() << "Websocket client: " << u;
 	connect(&socket, &QWebSocket::connected, this, &WebsocketClient::onConnected);
 	connect(&socket, &QWebSocket::disconnected, this, &WebsocketClient::closed);
-	socket.open(url);
+
+	socket.open(u);
 }
 
 void WebsocketClient::onConnected() {
 	qDebug() << "Websocket connected";
 	connect(&socket, &QWebSocket::textMessageReceived, this, &WebsocketClient::onTextMessageReceived);
 
-	//optional send
-	//socket.sendTextMessage("lalalalalalal");
+	socket.sendTextMessage("lalalalalalal");
 }
 
 void WebsocketClient::onTextMessageReceived(QString message) {
 	qDebug() << "WebsocketClient received: " << message;
-
+	static int i = 1;
+	QString str = message + QString::fromStdString(std::to_string(i));
+	socket.sendTextMessage(str);
 	//optional close
-	socket.close();
 }
