4dd4b68d572293cf6c837f9d241c0b689638d125
FL2137 <franek1519@gmail.com>
2024-03-11 23:24:36 +0100

    Every part of request should be in connects i guess

--- LoginDialogClass.cpp
+++ LoginDialogClass.cpp
@@ -17,39 +17,44 @@ LoginDialogClass::LoginDialogClass(QWidget* parent) : QDialog(parent) {
 
 		socket = new QWebSocket();
 
+		connect(socket, &QWebSocket::connected, this, [&]() {
 
-		json request;
-		request["type"] = "LOGIN";
-		request["uid"] = -1;
-		json data;
+			json request;
+			request["type"] = "LOGIN";
+			request["uid"] = -1;
+			json data;
 
-		data["nickname"] = ui.loginEdit->text().toStdString();
-		data["password"] = ui.passwordEdit->text().toStdString();
+			data["nickname"] = ui.loginEdit->text().toStdString();
+			data["password"] = ui.passwordEdit->text().toStdString();
 
-		request["data"] = data.dump();
+			request["data"] = data.dump();
 
-		json response;
 
-		connect(socket, &QWebSocket::textMessageReceived, this, [&](const QString& _response) {
-			response = json::parse(_response.toStdString());
-			if (response["ok"] == "OK") {
-				uid = response["uid"].get<int>();
-				emit passUid(uid);
-				dialog.close();
-			}
-			else {
-				ui.badCredsLabel->setText("Incorrect login or password");
-				ui.loginEdit->setText("");
-				ui.passwordEdit->setText("");
-			}
+			qDebug() << "sending: " << QString::fromStdString(request.dump());
+			socket->sendTextMessage(QString::fromStdString(request.dump()));
 
-			socket->close();
-			delete socket;
+
+			connect(socket, &QWebSocket::textMessageReceived, this, [&](const QString& _response) {
+				json response = json::parse(_response.toStdString());
+				if (response["ok"] == "OK") {
+					uid = response["uid"].get<int>();
+					emit passUid(uid);
+					dialog.close();
+				}
+				else {
+					ui.badCredsLabel->setText("Incorrect login or password");
+					ui.loginEdit->setText("");
+					ui.passwordEdit->setText("");
+				}
+
+				socket->close();
+				delete socket;
+			});
 		});
+
 		QUrl url("ws://192.168.1.109:3005");
 		socket->open(url);
-		qDebug() << "sending: " << QString::fromStdString(request.dump());
-		socket->sendTextMessage(QString::fromStdString(request.dump()));
+
 	});
 	dialog.show();
 	dialog.setFocus();
