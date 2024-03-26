260484eec588c2c70eb2876b15caf72b5c026bc3
FL2137 <franek1519@gmail.com>
2024-03-26 23:02:19 +0100

    HolePunching main function

--- HolePuncher.hpp
+++ HolePuncher.hpp
@@ -12,6 +12,57 @@ using boost::asio::ip::udp;
 namespace HP {
 
 
+	std::tuple<std::string, std::string> punchAhole(udp::socket* socket) {
+		auto stunAddress = boost::asio::ip::make_address("142.250.15.127"); //address of stun2.l.google.com
+
+		udp::endpoint stunEndpoint(stunAddress, 3748);
+
+		StunRequest stunRequest;
+		boost::system::error_code error;
+
+		socket->send_to(boost::asio::buffer(&stunRequest, sizeof(stunRequest)), stunEndpoint, 0, error);
+
+		if (error.value() != 0) {
+			std::cout << error.message() << std::endl;
+		}
+
+		udp::endpoint remoteEp;
+		std::array<char, 128> buffer;
+
+
+		int received = socket->receive_from(boost::asio::buffer(buffer), remoteEp, 0, error);
+		if (error.value() != 0) {
+			std::cout << error.message() << std::endl;
+		}
+
+		StunResponse* srsp = reinterpret_cast<StunResponse*>(&buffer);
+
+		if (srsp->magicCookie != stunRequest.magicCookie) {
+			std::cerr << "magicCookies do not match...\n";
+		}
+
+		for (int i = 0; i < 12; i++) {
+			std::cout << std::hex << (int)stunRequest.transactionID[i] << " - " << std::hex << (int)srsp->transactionID[i] << std::endl;
+		}
+
+		std::string address = "";
+		std::string port = "";
+
+		srsp->magicCookie = ntohl(srsp->magicCookie);
+
+		//parse XOR-MAPPED-ADDRESS
+		for (int i = 0; i < 4; i++) {
+			address += std::to_string(((srsp->magicCookie >> (24 - (i * 8))) & 0xff) ^ ((ntohl(srsp->attribXORMAPPEDADDRESS.address) >> 24 - (i * 8)) & 0xff)) + ".";
+		}
+		address.pop_back();//get rid of the last dot
+
+		port = std::to_string((ntohl(srsp->attribXORMAPPEDADDRESS.port) & 0xffFF) ^ (srsp->magicCookie >> 16));
+
+		return std::make_tuple(address, port);
+
+		
+	}
+
 
 #pragma pack(push, 1)
 	struct StunRequest {
