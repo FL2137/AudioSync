0f3e45e2ce62f6c884fc2e97e3e439622e5fc630
FL2137 <franek1519@gmail.com>
2024-03-29 22:37:48 +0100

    Is now a class....

--- HolePuncher.hpp
+++ HolePuncher.hpp
@@ -10,10 +10,56 @@
 
 using boost::asio::ip::udp;
 
-namespace HP {
+class HolePuncher {
+
+#pragma pack(push, 1)
+	struct StunRequest {
+		StunRequest() {
+
+			//generate random transaction ID
+
+			std::random_device device;
+			std::mt19937 generator(device());
+			std::uniform_int_distribution<int> distribution(0, 255);
+
+			//yh
+			srand(time(NULL));
+
+			for (int i = 0; i < 12; i++) {
+				transactionID[i] = rand() % 256;
+			}
+		}
+
+		const int16_t messageType = htons(0x0001);
+		const int16_t messageLen = htons(0x0000);
+		const int32_t magicCookie = htonl(0x2112A442);
+		std::array<unsigned char, 12> transactionID = { 0 };
+	};
+#pragma pack(pop)
 
 
-	std::tuple<std::string, std::string> punchAhole(std::unique_ptr<udp::socket> &socket) {
+#pragma pack(push, 1)
+	struct StunAttribute {
+		int16_t type;
+		int16_t len;
+		int32_t port;
+		int32_t address;
+	};
+#pragma pack(pop)
+
+
+#pragma pack(push, 1)
+	struct StunResponse {
+		int16_t messageType;
+		int16_t messageLen;
+		int32_t magicCookie;
+		std::array<unsigned char, 12> transactionID;
+		StunAttribute attribXORMAPPEDADDRESS;
+	};
+#pragma pack(pop)
+
+
+	std::tuple<std::string, std::string> punchAhole(std::unique_ptr<udp::socket>& socket) {
 		auto stunAddress = boost::asio::ip::make_address("142.250.15.127"); //address of stun2.l.google.com
 
 		udp::endpoint stunEndpoint(stunAddress, 3748);
@@ -61,53 +107,6 @@ namespace HP {
 
 		return std::make_tuple(address, port);
 
-		
+
 	}
-
-
-#pragma pack(push, 1)
-	struct StunRequest {
-		StunRequest() {
-
-			//generate random transaction ID
-
-			std::random_device device;
-			std::mt19937 generator(device());
-			std::uniform_int_distribution<int> distribution(0, 255);
-
-			//yh
-			srand(time(NULL));
-
-			for (int i = 0; i < 12; i++) {
-				transactionID[i] = rand() % 256;
-			}
-		}
-
-		const int16_t messageType = htons(0x0001);
-		const int16_t messageLen = htons(0x0000);
-		const int32_t magicCookie = htonl(0x2112A442);
-		std::array<unsigned char, 12> transactionID = { 0 };
-	};
-#pragma pack(pop)
-
-
-#pragma pack(push, 1)
-	struct StunAttribute {
-		int16_t type;
-		int16_t len;
-		int32_t port;
-		int32_t address;
-	};
-#pragma pack(pop)
-
-
-#pragma pack(push, 1)
-	struct StunResponse {
-		int16_t messageType;
-		int16_t messageLen;
-		int32_t magicCookie;
-		std::array<unsigned char, 12> transactionID;
-		StunAttribute attribXORMAPPEDADDRESS;
-	};
-#pragma pack(pop)
-}
+};
