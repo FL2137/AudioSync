63dc79668594883f84f3b275e21156c1df2d2e11
FL2137 <franek1519@gmail.com>
2024-03-31 22:22:52 +0200

    Some hole punching and sending the result to server

--- Session.cpp
+++ Session.cpp
@@ -4,6 +4,31 @@
 Session::Session() {
 	audioCapture = new AudioHandler(AudioHandler::MODE::CAPTURE);
 	audioRender = new AudioHandler(AudioHandler::MODE::RENDER);
+
+
+	auto _socket = audioRender->getSocket();
+	
+	auto endpointData = HolePuncher::punchAhole(_socket); //this lux bad
+
+	qDebug() << "after hole punching";
+		
+	json request;
+
+	request["type"] = "SET_ENDPOINT";
+	request["uid"] = this->uid;
+	request["address"] = std::get<0>(endpointData);
+	request["port"] = std::get<1>(endpointData);
+
+	std::string res;
+
+	BeastClient::syncBeast(request.dump(), res);
+
+	json response = json::parse(res);
+	
+	if (response["ok"] == "OK") {
+		//yay, can start our session
+	}
+
 	renderBuffer = new char[BUFFERSIZE] {0};
 }
 
