fda40713d381b1c5adf54114ef6554c6d55fca02
FL2137 <franek1519@gmail.com>
2024-04-15 22:27:47 +0200

    Small cleanup

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include "AudioSync.h"
-#include "AudioHandler.hpp"
 #include <QtWidgets/QApplication>
-#include "HolePuncher.hpp"
-
-
-
-
 
 int main(int argc, char *argv[]) {
-
-
-
-
-
-
 
     QApplication a(argc, argv);
     AudioSync w;
     w.show();
-
+    
     return a.exec();
 }
